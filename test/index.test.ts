553fd45ff8ab4f90d5be5a51cd336d742a460e7d
Brian Cullen <brian.cullen@gmail.com>
2019-10-26 22:20:17 +0100

    Add Initial Interface
    Also changed from tslint to eslint.

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,8 +1,44 @@
-import insulter from '../src'
+import TextractParser from '../src'
+import { Textract, AWSError } from 'aws-sdk'
 
-describe('Insult test', () => {
-    it('Should insult', () => {
-        const insult = insulter()
-        expect(insult).toBe(`You're ugly!`)
+describe('Should handle textract callback', () => {
+  it('Should passthrough error without alteration', (done) => {
+    const error = {}
+    const callback = TextractParser.handleDetectTextCallback((err, data) => {
+      expect(err).toBe(error)
+      expect(data).toBeNull()
+      done()
     })
+
+    callback(error as AWSError, null)
+  })
+
+  it('Should return empty object on parsing', (done) => {
+    const result = {}
+    const response: Textract.DetectDocumentTextResponse = {
+      DocumentMetadata: { Pages: 1 },
+      Blocks: []
+    }
+
+    const callback = TextractParser.handleDetectTextCallback((err, data) => {
+      expect(err).toBeNull()
+      expect(data).toEqual(result)
+      done()
+    })
+
+    callback(null, response)
+  })
+})
+
+describe('Should handle textract response', () => {
+  it('Should return empty object on parsing', () => {
+    const expectedResult = {}
+    const response: Textract.DetectDocumentTextResponse = {
+      DocumentMetadata: { Pages: 1 },
+      Blocks: []
+    }
+
+    const result = TextractParser.parseDetectTextResponse(response)
+    expect(result).toEqual(expectedResult)
+  })
 })
