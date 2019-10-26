fb5867a255c31363d6172a94f2a5d2d242966620
Brian Cullen <brian.cullen@cdl.co.uk>
2019-10-26 15:54:17 +0100

    Remove Other Code

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,4 @@ function insulter(): string {
   return `You're ugly!`;
 }
 
-export const doSomething = (): string => {
-  const target = "there";
-  const message = "hello";
-  return [message, target].join(" ");
-};
-
-export const doSomethingElse = (): string => {
-  const target = "there";
-  const message = "hello";
-  return [message, target].join(" ");
-};
-
 export default insulter;
