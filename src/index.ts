ca3f220d476bf0489d8a796e9ee3659f603c8b14
Brian Cullen <brian.cullen@cdl.co.uk>
2019-10-26 22:15:00 +0100

    Add Initial Interface
    Also changed from tslint to eslint.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
-function insulter(): string {
-  return `You're ugly!`;
+import { AWSError, Textract } from 'aws-sdk'
+
+type ParsedDetectTextCallback = (err: AWSError | null, data: object | null) => void
+type TextractDetectTextCallback = (err: AWSError | null, data: Textract.Types.DetectDocumentTextResponse | null) => void
+
+function parseDetectTextResponse (response: Textract.DetectDocumentTextResponse): object {
+  return { }
 }
 
-export default insulter;
+function handleDetectTextCallback (callback: ParsedDetectTextCallback): TextractDetectTextCallback {
+  return (err, detectTextResponse): void => {
+    if (err !== null || detectTextResponse === null) {
+      callback(err, detectTextResponse)
+    } else {
+      callback(err, parseDetectTextResponse(detectTextResponse))
+    }
+  }
+}
+
+export default {
+  handleDetectTextCallback,
+  parseDetectTextResponse
+}
